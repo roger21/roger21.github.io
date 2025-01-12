90945f728a6a3f6e18e03ecc619bd7ccf401e785
roger21 <roger21@free.fr>
2025-01-12 09:48:36 +0100

    hop

--- stats/ukr/__gnuplot.sh
+++ stats/ukr/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls ukr2_20240104132943_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! UKR2" ; else echo "no ukr2_20240104132943_00_data file" ; exit 1 ; fi
+if ls ukr2_20250111093228_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! UKR2" ; else echo "no ukr2_20250111093228_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed UKR2 :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/ukr/ukr2_20240104132943_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed UKR2 :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/ukr/ukr2_20250111093228_00_gnuplot.gp
 
-sed -i "s/<title>ukr2_20240104132943_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2023 du topic Ukraine<\/title>/g" ukr2_20240104132943_g1_counts_all.svg
-sed -i "s/<title>ukr2_20240104132943_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2023 du topic Ukraine<\/title>/g" ukr2_20240104132943_g2_evol_all.svg
+sed -i "s/<title>ukr2_20250111093228_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic Ukraine<\/title>/g" ukr2_20250111093228_g1_counts_all.svg
+sed -i "s/<title>ukr2_20250111093228_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic Ukraine<\/title>/g" ukr2_20250111093228_g2_evol_all.svg
 
-rm ukr2_20240104132943_00_data_*
+rm ukr2_20250111093228_00_data_*
 
