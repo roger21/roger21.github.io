696e6dcd9d7b17ed852ae5de9ad4a8f12c39ad91
roger21 <roger21@free.fr>
2025-01-12 09:44:31 +0100

    hop

--- stats/gsn/__gnuplot.sh
+++ stats/gsn/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls gsn_20240104062100_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! GSN" ; else echo "no gsn_20240104062100_00_data file" ; exit 1 ; fi
+if ls gsn_20250111044545_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! GSN" ; else echo "no gsn_20250111044545_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed GSN :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/gsn/gsn_20240104062100_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed GSN :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/gsn/gsn_20250111044545_00_gnuplot.gp
 
-sed -i "s/<title>gsn_20240104062100_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2023 du topic GSNALF<\/title>/g" gsn_20240104062100_g1_counts_all.svg
-sed -i "s/<title>gsn_20240104062100_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2023 du topic GSNALF<\/title>/g" gsn_20240104062100_g2_evol_all.svg
+sed -i "s/<title>gsn_20250111044545_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic GSNALF<\/title>/g" gsn_20250111044545_g1_counts_all.svg
+sed -i "s/<title>gsn_20250111044545_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic GSNALF<\/title>/g" gsn_20250111044545_g2_evol_all.svg
 
-rm gsn_20240104062100_00_data_*
+rm gsn_20250111044545_00_data_*
 
