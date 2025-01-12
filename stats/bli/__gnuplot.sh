2b61a2b51836d357587ade667116e2553ee39c98
roger21 <roger21@free.fr>
2025-01-12 09:41:15 +0100

    hop

--- stats/bli/__gnuplot.sh
+++ stats/bli/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls bli_20240104002257_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! BLI" ; else echo "no bli_20240104002257_00_data file" ; exit 1 ; fi
+if ls bli_20250110235119_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! BLI" ; else echo "no bli_20250110235119_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed BLI :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/bli/bli_20240104002257_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed BLI :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/bli/bli_20250110235119_00_gnuplot.gp
 
-sed -i "s/<title>bli_20240104002257_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2023 du topic blind test<\/title>/g" bli_20240104002257_g1_counts_all.svg
-sed -i "s/<title>bli_20240104002257_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2023 du topic blind test<\/title>/g" bli_20240104002257_g2_evol_all.svg
+sed -i "s/<title>bli_20250110235119_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic blind test<\/title>/g" bli_20250110235119_g1_counts_all.svg
+sed -i "s/<title>bli_20250110235119_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic blind test<\/title>/g" bli_20250110235119_g2_evol_all.svg
 
-rm bli_20240104002257_00_data_*
+rm bli_20250110235119_00_data_*
 
