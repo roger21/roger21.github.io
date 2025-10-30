be9ef0e21495a6b0121f5a8a824ec87dd1762eb2
roger21 <roger21@free.fr>
2025-10-30 17:31:04 +0100

    hop

---
+++ stats/lrm/__gnuplot.sh
@@ -0,0 +1,9 @@
+if ls lrm_20251030143511_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! LRM" ; else echo "no lrm_20251030143511_00_data file" ; exit 1 ; fi
+
+/usr/bin/time -f "\tElapsed LRM :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/lrm/lrm_20251030143511_00_gnuplot.gp
+
+sed -i "s/<title>lrm_20251030143511_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic LREM<\/title>/g" lrm_20251030143511_g1_counts_all.svg
+sed -i "s/<title>lrm_20251030143511_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic LREM<\/title>/g" lrm_20251030143511_g2_evol_all.svg
+
+rm lrm_20251030143511_00_data_*
+
