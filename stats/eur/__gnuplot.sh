4e63463bd1e56de422ee23480084a0d5051c5348
roger21 <roger21@free.fr>
2025-11-09 17:59:25 +0100

    hop

---
+++ stats/eur/__gnuplot.sh
@@ -0,0 +1,9 @@
+if ls eur_20251109171956_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! EUR" ; else echo "no eur_20251109171956_00_data file" ; exit 1 ; fi
+
+/usr/bin/time -f "\tElapsed EUR :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/eur/eur_20251109171956_00_gnuplot.gp
+
+sed -i "s/<title>eur_20251109171956_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic UE\/Europe<\/title>/g" eur_20251109171956_g1_counts_all.svg
+sed -i "s/<title>eur_20251109171956_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic UE\/Europe<\/title>/g" eur_20251109171956_g2_evol_all.svg
+
+rm eur_20251109171956_00_data_*
+
