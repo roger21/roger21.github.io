7815c2d78bfd168025a129da361d193bb9108b38
roger21 <roger21@free.fr>
2026-01-10 13:36:40 +0100

    hop

--- stats/rep/__gnuplot.sh
+++ stats/rep/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls rep_20251108081745_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! REP" ; else echo "no rep_20251108081745_00_data file" ; exit 1 ; fi
+if ls rep_20260106180925_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! REP" ; else echo "no rep_20260106180925_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed REP :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/rep/rep_20251108081745_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed REP :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/rep/rep_20260106180925_00_gnuplot.gp
 
-sed -i "s/<title>rep_20251108081745_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic Les Républicains<\/title>/g" rep_20251108081745_g1_counts_all.svg
-sed -i "s/<title>rep_20251108081745_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic Les Républicains<\/title>/g" rep_20251108081745_g2_evol_all.svg
+sed -i "s/<title>rep_20260106180925_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2025 du topic Les Républicains<\/title>/g" rep_20260106180925_g1_counts_all.svg
+sed -i "s/<title>rep_20260106180925_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2025 du topic Les Républicains<\/title>/g" rep_20260106180925_g2_evol_all.svg
 
-rm rep_20251108081745_00_data_*
+rm rep_20260106180925_00_data_*
 
