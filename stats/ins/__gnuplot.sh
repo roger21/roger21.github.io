9e7248eaf234dd2e533181289c65825f22443d77
roger21 <roger21@free.fr>
2026-01-10 13:34:36 +0100

    hop

--- stats/ins/__gnuplot.sh
+++ stats/ins/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls ins_20250111072313_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! INS" ; else echo "no ins_20250111072313_00_data file" ; exit 1 ; fi
+if ls ins_20260106065441_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! INS" ; else echo "no ins_20260106065441_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed INS :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/ins/ins_20250111072313_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed INS :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/ins/ins_20260106065441_00_gnuplot.gp
 
-sed -i "s/<title>ins_20250111072313_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic insomniaks<\/title>/g" ins_20250111072313_g1_counts_all.svg
-sed -i "s/<title>ins_20250111072313_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic insomniaks<\/title>/g" ins_20250111072313_g2_evol_all.svg
+sed -i "s/<title>ins_20260106065441_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2025 du topic insomniaks<\/title>/g" ins_20260106065441_g1_counts_all.svg
+sed -i "s/<title>ins_20260106065441_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2025 du topic insomniaks<\/title>/g" ins_20260106065441_g2_evol_all.svg
 
-rm ins_20250111072313_00_data_*
+rm ins_20260106065441_00_data_*
 
