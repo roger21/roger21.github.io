c39a39c87b13a973496f1a660ef4b1c0c3c22dbf
roger21 <roger21@free.fr>
2026-01-10 13:31:45 +0100

    hop

--- stats/cov/__gnuplot.sh
+++ stats/cov/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls cov2_20250111203524_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! COV2" ; else echo "no cov2_20250111203524_00_data file" ; exit 1 ; fi
+if ls cov2_20260105172630_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! COV2" ; else echo "no cov2_20260105172630_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed COV2 :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/cov/cov2_20250111203524_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed COV2 :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/cov/cov2_20260105172630_00_gnuplot.gp
 
-sed -i "s/<title>cov2_20250111203524_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic COVID-19<\/title>/g" cov2_20250111203524_g1_counts_all.svg
-sed -i "s/<title>cov2_20250111203524_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic COVID-19<\/title>/g" cov2_20250111203524_g2_evol_all.svg
+sed -i "s/<title>cov2_20260105172630_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2025 du topic COVID-19<\/title>/g" cov2_20260105172630_g1_counts_all.svg
+sed -i "s/<title>cov2_20260105172630_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2025 du topic COVID-19<\/title>/g" cov2_20260105172630_g2_evol_all.svg
 
-rm cov2_20250111203524_00_data_*
+rm cov2_20260105172630_00_data_*
 
