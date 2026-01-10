c834ab5f536c2db8c009396516b52b06223d628b
roger21 <roger21@free.fr>
2026-01-10 13:35:50 +0100

    hop

--- stats/nat/__gnuplot.sh
+++ stats/nat/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls nat_20251109103334_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! NAT" ; else echo "no nat_20251109103334_00_data file" ; exit 1 ; fi
+if ls nat_20260106133723_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! NAT" ; else echo "no nat_20260106133723_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed NAT :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/nat/nat_20251109103334_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed NAT :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/nat/nat_20260106133723_00_gnuplot.gp
 
-sed -i "s/<title>nat_20251109103334_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic du Rassemblement national<\/title>/g" nat_20251109103334_g1_counts_all.svg
-sed -i "s/<title>nat_20251109103334_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic du Rassemblement national<\/title>/g" nat_20251109103334_g2_evol_all.svg
+sed -i "s/<title>nat_20260106133723_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2025 du topic du Rassemblement national<\/title>/g" nat_20260106133723_g1_counts_all.svg
+sed -i "s/<title>nat_20260106133723_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2025 du topic du Rassemblement national<\/title>/g" nat_20260106133723_g2_evol_all.svg
 
-rm nat_20251109103334_00_data_*
+rm nat_20260106133723_00_data_*
 
