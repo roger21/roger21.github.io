a32323653fd3053da00559dcc67aadae46ccd729
roger21 <roger21@free.fr>
2026-01-10 13:38:02 +0100

    hop

--- stats/zem/__gnuplot.sh
+++ stats/zem/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls zem2_20250111103244_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! ZEM2" ; else echo "no zem2_20250111103244_00_data file" ; exit 1 ; fi
+if ls zem2_20260106233358_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! ZEM2" ; else echo "no zem2_20260106233358_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed ZEM2 :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/zem/zem2_20250111103244_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed ZEM2 :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/zem/zem2_20260106233358_00_gnuplot.gp
 
-sed -i "s/<title>zem2_20250111103244_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic Zemmour<\/title>/g" zem2_20250111103244_g1_counts_all.svg
-sed -i "s/<title>zem2_20250111103244_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic Zemmour<\/title>/g" zem2_20250111103244_g2_evol_all.svg
+sed -i "s/<title>zem2_20260106233358_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2025 du topic Zemmour<\/title>/g" zem2_20260106233358_g1_counts_all.svg
+sed -i "s/<title>zem2_20260106233358_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2025 du topic Zemmour<\/title>/g" zem2_20260106233358_g2_evol_all.svg
 
-rm zem2_20250111103244_00_data_*
+rm zem2_20260106233358_00_data_*
 
