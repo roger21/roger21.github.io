3e8dc52b1dc0efbc67a4bf05b8afd4e5cdb34329
roger21 <roger21@free.fr>
2026-01-10 13:35:31 +0100

    hop

--- stats/met/__gnuplot.sh
+++ stats/met/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls met_20250706132342_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! MET" ; else echo "no met_20250706132342_00_data file" ; exit 1 ; fi
+if ls met_20260106130037_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! MET" ; else echo "no met_20260106130037_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed MET :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/met/met_20250706132342_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed MET :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/met/met_20260106130037_00_gnuplot.gp
 
-sed -i "s/<title>met_20250706132342_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic Météo<\/title>/g" met_20250706132342_g1_counts_all.svg
-sed -i "s/<title>met_20250706132342_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic Météo<\/title>/g" met_20250706132342_g2_evol_all.svg
+sed -i "s/<title>met_20260106130037_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2025 du topic Météo<\/title>/g" met_20260106130037_g1_counts_all.svg
+sed -i "s/<title>met_20260106130037_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2025 du topic Météo<\/title>/g" met_20260106130037_g2_evol_all.svg
 
-rm met_20250706132342_00_data_*
+rm met_20260106130037_00_data_*
 
