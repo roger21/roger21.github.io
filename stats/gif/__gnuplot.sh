794a4c6d6e4bf3176238200a392eb483bc8ef5c6
roger21 <roger21@free.fr>
2026-01-10 13:33:19 +0100

    hop

--- stats/gif/__gnuplot.sh
+++ stats/gif/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls gif_20250111043648_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! GIF" ; else echo "no gif_20250111043648_00_data file" ; exit 1 ; fi
+if ls gif_20260106012448_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! GIF" ; else echo "no gif_20260106012448_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed GIF :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/gif/gif_20250111043648_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed GIF :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/gif/gif_20260106012448_00_gnuplot.gp
 
-sed -i "s/<title>gif_20250111043648_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic Gifs<\/title>/g" gif_20250111043648_g1_counts_all.svg
-sed -i "s/<title>gif_20250111043648_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic Gifs<\/title>/g" gif_20250111043648_g2_evol_all.svg
+sed -i "s/<title>gif_20260106012448_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2025 du topic Gifs<\/title>/g" gif_20260106012448_g1_counts_all.svg
+sed -i "s/<title>gif_20260106012448_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2025 du topic Gifs<\/title>/g" gif_20260106012448_g2_evol_all.svg
 
-rm gif_20250111043648_00_data_*
+rm gif_20260106012448_00_data_*
 
