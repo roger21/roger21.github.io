f408854589324788c7701a10a29d8ada37c3959e
roger21 <roger21@free.fr>
2026-01-10 13:35:24 +0100

    hop

--- stats/lux/__gnuplot.sh
+++ stats/lux/__gnuplot.sh
@@ -1,9 +1,9 @@
-if ls lux2_20250111084206_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! LUX2" ; else echo "no lux2_20250111084206_00_data file" ; exit 1 ; fi
+if ls lux2_20260106125338_00_data_* 1>/dev/null 2>&1 ; then echo "lessgo! LUX2" ; else echo "no lux2_20260106125338_00_data file" ; exit 1 ; fi
 
-/usr/bin/time -f "\tElapsed LUX2 :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/lux/lux2_20250111084206_00_gnuplot.gp
+/usr/bin/time -f "\tElapsed LUX2 :\t%E" /usr/bin/gnuplot /home/moi/devs/gittop/stats/lux/lux2_20260106125338_00_gnuplot.gp
 
-sed -i "s/<title>lux2_20250111084206_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2024 du topic Luxembourg<\/title>/g" lux2_20250111084206_g1_counts_all.svg
-sed -i "s/<title>lux2_20250111084206_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2024 du topic Luxembourg<\/title>/g" lux2_20250111084206_g2_evol_all.svg
+sed -i "s/<title>lux2_20260106125338_g1_counts_all<\/title>/<title>Le nombre de posts et de participants all time 2025 du topic Luxembourg<\/title>/g" lux2_20260106125338_g1_counts_all.svg
+sed -i "s/<title>lux2_20260106125338_g2_evol_all<\/title>/<title>L'évolution du nombre de participants all time 2025 du topic Luxembourg<\/title>/g" lux2_20260106125338_g2_evol_all.svg
 
-rm lux2_20250111084206_00_data_*
+rm lux2_20260106125338_00_data_*
 
